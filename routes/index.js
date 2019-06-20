e8d35f5bc2277174aba54c3ef81364184a75ebae
Roqak <roqak1@gmail.com>
2019-06-20 14:44:36 +0100

    added support for http,https and www

--- routes/index.js
+++ routes/index.js
@@ -2,21 +2,47 @@ const express = require('express')
 const Router = express.Router()
 const randomString = require('random-string');
 const url = require('../Model/Url');
+let finalurl = "";
+const checkForHTTPS = (url)=>{
+    let splitted = url.split("//");
+    console.log(splitted);
+    if(splitted[0] == "https:" || splitted[0] == "http:"){
+        finalurl = splitted[1];
+        // tempurl = finalurl.split("")
+        // finalurl = finalurl[0]
+        console.log(true)
+    }else{
+        finalurl = url;
+        console.log(false)
+    }
+    // console.log(`url: ${finalurl}`)
+    return finalurl;
+}
 // const x = randomString({length: 5});
 Router.get('/',(req,res)=>{
     res.render("home")
 })
-
+// console.log("http".equals("https"))
 Router.get('/:url',(req,res)=>{
     // res.send(req.params.url)
     // console.log(`https://www.${req.params.url}`)
+    let conn = "";
         url.find({shortUrl:req.params.url})
         .then(result=>{
             // res.send(result)
             if(result.length < 1){
                 res.send("url not found")
             }else{
-                res.redirect(`http://www.${result[0].url}`)
+                if(result[0].url.startsWith('www.')){
+                    conn = result[0].url.slice(4);
+                    // console.log(`conn: ${result[0].url.startsWith('www.')}`)
+                }else{
+                    conn = result[0].url
+                    // console.log(`conn: ${result[0].url.startsWith('www.')}`)
+
+                }
+                // res.send(conn)
+                res.redirect(`http://${conn}`)
             }
         })
         .catch(err=>{
@@ -56,8 +82,19 @@ Router.get('/s/:userString/:url',(req,res)=>{
 })
 
 Router.post('/s',(req,res)=>{
+    checkForHTTPS(req.body.url);
+    // let splitted = req.body.url.split("//");
+    // console.log(splitted);
+    // if(splitted[0] == "https:" || splitted[0] == "http:"){
+    //     finalurl = splitted[1];
+    //     console.log(true)
+    // }else{
+    //     finalurl = req.body.url;
+    //     console.log(false)
+    // }
+    // console.log(`url: ${finalurl}`)
     const newUrl = new url({
-        url : req.body.url,
+        url : checkForHTTPS(req.body.url),
         shortUrl: randomString({length: 6}).toLowerCase()
     })
     // res.send(randomString({length: 6}).toLowerCase())
